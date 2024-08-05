1c63b42e52c832d078ac8dfdb61ee75a68c46051
WMDA <mszdjh3@nottingham.ac.uk>
2024-08-05 17:15:32 +0100

    working on dual regression

--- NFACT/regression/dual_regression.py
+++ NFACT/regression/dual_regression.py
@@ -1,43 +1,54 @@
-from sklearn.preprocessing import StandardScaler
+from NFACT.decomposition.decomp import normalise_components
 import numpy as np
+from scipy.optimize import nnls
 
 
-def dualreg(Cs, X, normalise=False):
-    """Dual regression of C on X where C is a group mat and X is either G or W
-    We have :  C = G*W
-    So either:
-        Cs = G*Ws
-    or:
-        Cs' = W'*Gs'
-    Input:
-    Cs: 2d array - individual subject connectivity matrix
-    Outputs:
-    Gs : 2d array
-    Ws : 2d array
+def dualreg(
+    connectivity_matrix: np.array,
+    components: dict,
+    algo: str,
+    normalise: str,
+    glm: bool = False,
+):
     """
-    # if X is G, then X.shape[0] is same as Cs.shape[0]
-    if X.shape[0] == Cs.shape[0]:
-        # X is G
-        Ws = np.linalg.pinv(X) @ Cs
-        Gs = (np.linalg.pinv(Ws.T) @ Cs.T).T
-    # if X is W, then X.shape[1] is same as Cs.shape[1] (remember W is in fact W.T)
-    elif X.shape[1] == Cs.shape[1]:
-        # X is W
-        Gs = (np.linalg.pinv(X.T) @ Cs.T).T
-        Ws = np.linalg.pinv(Gs) @ Cs
+    Wrapper function to perform dual regression
+    and save results
 
+    Parameters
+    ----------
+    connectivity_matrix: np.array
+        connectivity matrix of a single subject
+    component: dict
+        dictionary of components
+    """
+
+    if algo == "ica":
+        dual_reg = ICA_dual_regression(connectivity_matrix, components)
+    if algo == "nfm":
+        return
     if normalise:
-        Gs = StandardScaler().fit_transform(Gs)
-        Ws = StandardScaler().fit_transform(Ws.T).T
+        demean = True if algo == "ica" else False
+        normalise_components(dual_reg["grey_matter"], dual_reg["white_matter"], demean)
 
-    return Gs, Ws
+    if glm:
+        return dual_reg
+
+
+class Dual_regression:
+    def __init__(self, list_of_file, algo, normalise) -> None:
+        self.algo = algo
+        self.normalise = normalise
+
+
+def nfm_dual_regression(connectivity_matrix, component):
+    return np.array([nnls(A, B[:, i])[0] for i in range(B.shape[1])]).T
 
 
 # def
 def ICA_dual_regression(
     connectivity_matrix: np.array,
-    component: np.array,
-) -> np.darray:
+    component: dict,
+) -> dict:
     """
     Dual regression function for ICA.
     Regresses the invidiual connectivity matrix
@@ -46,22 +57,32 @@ def ICA_dual_regression(
     If white matter component then regresses
     grey matter map onto connectivity matrix and vice versa.
 
+    Parameters
+    ----------
+    connectivity_matrix: np.array
+        connectivity matrix of a single subject
+    component: dict
+        dictionary of components
+
+    Returns
+    -------
+    dictionary: dict
+        dictionary of grey and white matter components
+
 
     """
-    wm_component_grey_weights = (
+    wm_component_grey_map = (
         np.linalg.pinv(component["white_components"].T) @ connectivity_matrix.T
     ).T
-    wm_component_white_weights = (
-        np.linalg.pinv(wm_component_grey_weights) @ connectivity_matrix
-    )
-    gm_component_white_weights = (
+    wm_component_white_map = np.linalg.pinv(wm_component_grey_map) @ connectivity_matrix
+    gm_component_white_map = (
         np.linalg.pinv(component["grey_components"]) @ connectivity_matrix
     )
-    gm_component_grey_weights = (
-        np.linalg.pinv(gm_component_white_weights.T) @ connectivity_matrix.T
+    gm_component_grey_map = (
+        np.linalg.pinv(gm_component_white_map.T) @ connectivity_matrix.T
     ).T
 
     return {
-        "white_matter": wm_component_white_weights,
-        "grey_matter": gm_component_grey_weights,
+        "white_matter": wm_component_white_map,
+        "grey_matter": gm_component_grey_map,
     }
