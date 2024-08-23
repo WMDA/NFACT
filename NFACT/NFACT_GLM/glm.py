d8ca177d98fbfa19ca7604ab496909571f478804
WMDA <mszdjh3@nottingham.ac.uk>
2024-08-23 13:10:23 +0100

    working on the GLM

--- NFACT/NFACT_GLM/glm.py
+++ NFACT/NFACT_GLM/glm.py
@@ -5,36 +5,65 @@ from scipy.stats import t
 
 # GLM Stuff
 class GLM:
-    def __init__(self):
-        self.beta_ = None
-        self.res_ = None
-        self.dof_ = None
-        self.sigma_sq_ = None
-        self.XtX_ = None
+    """
+    Class to peform regression on
 
-    def fit(self, X, y):
-        self.beta_ = np.linalg.pinv(X) @ y
-        self.res_ = y - X @ self.beta_
-        self.dof_ = self.res_.shape[0] - np.linalg.matrix_rank(X)
-        self.sigma_sq_ = np.sum(self.res_**2, axis=0, keepdims=True) / self.dof_
-        self.XtX_ = X.T @ X
+    Usage
+    -----
+    """
 
-    def calc_stats(self, C):
-        if self.beta_ is not None:
-            C = np.array(C)
-            cope = C @ self.beta_
-            varcope = np.outer(np.diag(C @ self.XtX_ @ C.T), self.sigma_sq_)
-            # t-stat
-            tstat = cope / np.sqrt(varcope)
-            pval = 1 - t.cdf(tstat, self.dof_)
-            return {"tstat": tstat, "pval": pval, "zstat": self.ttoz(tstat, self.dof_)}
+    def __init__(
+        self,
+    ):
+        self.X = None
+        self.y = None
+        self.beta = None
+        self.dof = None
+        self.sigma_sq = None
+        # self.XtX_ = None
 
-    @staticmethod
-    def ttoz(t, dof):
-        def tdist(t, v):
-            return betainc(v / 2.0, 1 / 2, v / (v + t**2)) / 2.0
+    def fit(self, X: np.array, y: np.array):
+        self.X = X
+        self.y = y
+        self.beta = self.__beta()
+        res = self.__residuals()
+        self.dof = self.__dof()
+        self.sigma_sq_ = self.__sigma_sq(res)
+        self.XtX_ = self.__XtX()
 
-        return (np.sqrt(2) * erfinv(1 - 2 * tdist(t, dof))) * np.sign(t)
+    def __XtX(self):
+        return self.X.T @ self.X
+
+    def __sigma_sq(self, res):
+        return np.sum(res**2, axis=0, keepdims=True) / self.dof
+
+    def __residuals(self):
+        return self.y - self.X @ self.beta
+
+    def __dof(self):
+        return self.X.shape[0] - np.linalg.matrix_rank(self.X)
+
+    def __beta(self):
+        return np.linalg.pinv(self.X) @ self.y
+
+    def results(self, contrast: np.array):
+        cope = contrast @ self.beta
+        varcope = np.outer(np.diag(contrast @ self.XtX_ @ contrast.T), self.sigma_sq_)
+        tstat = self.__t_statistic(cope, varcope)
+        pval = self.__pval(tstat)
+        return {"tstat": tstat, "pval": pval, "zstat": self.__zstat(tstat, self.dof_)}
+
+    def __t_statistic(self, cope, varcope):
+        return cope / np.sqrt(varcope)
+
+    def __pval(self, tstat):
+        return 2 * (t.cdf(np.abs(tstat), self.dof))
+
+    def __zstat(self, tstat, dof):
+        beta_func = (
+            betainc(self.dof / 2.0, 1 / 2, self.dof / (self.dof + tstat**2)) / 2.0
+        )
+        return (np.sqrt(2) * erfinv(1 - 2 * beta_func)) * np.sign(tstat)
 
     def fit_transform(self, X, y):
         self.fit(X, y)
--- NFACT/NFACT_GLM/nfact_glm_setup.py
+++ NFACT/NFACT_GLM/nfact_glm_setup.py
@@ -1,6 +1,26 @@
 from NFACT.NFACT_base.utils import error_and_exit
 
 
-def check_complusory_args(args: dict) -> None:
-    complusory_args = []
-    return None
+def check_compulsory_arguments(args: dict) -> None:
+    """
+    Function to check that complusory arguments
+    are provided.
+
+    Parameters
+    ----------
+    args: dict
+        A dictionary of args
+
+    Returns
+    -------
+    None
+    """
+
+    compulsory_arguments = ["nfact_dir", "type_of_decomp", "design_matrix", "contrasts"]
+
+    [
+        error_and_exit(
+            args[key], f"{key} is not provided. Please use --{key} to specify"
+        )
+        for key in compulsory_arguments
+    ]
