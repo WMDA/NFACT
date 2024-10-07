6917679320c9c46ee4420a8105fc349d2b530873
WMDA <mszdjh3@nottingham.ac.uk>
2024-10-07 09:38:26 +0100

    fixed nfact_pp error that stop yupdating seeds

--- NFACT/preprocess/nfactpp.py
+++ NFACT/preprocess/nfactpp.py
@@ -70,11 +70,8 @@ def pre_processing(arg: dict, handler) -> None:
         seed_text = "\n".join(seed)
         # using this function not to return a file but check it is an imaging file
         get_file(arg["warps"], sub)
-
         nfactpp_diretory = os.path.join(arg["outdir"], "nfact_pp", sub_id)
-
         nfact_pp_folder_setup(nfactpp_diretory)
-
         [
             shutil.copyfile(
                 seed_location,
@@ -84,7 +81,6 @@ def pre_processing(arg: dict, handler) -> None:
             )
             for seed_location in seed
         ]
-
         if arg["surface"]:
             roi = get_file(arg["rois"], sub)
             seed_names = [
@@ -101,9 +97,7 @@ def pre_processing(arg: dict, handler) -> None:
                 for seed in seed_names
             ]
             seed_text = "\n".join(asc_seeds)
-
         error_and_exit(write_options_to_file(nfactpp_diretory, seed_text))
-
         if not arg["target2"]:
             print(
                 f'{col["purple"]}No target given. Creating a whole brain target.{col["reset"]}'
@@ -115,7 +109,6 @@ def pre_processing(arg: dict, handler) -> None:
                 arg["ref"],
                 "nearestneighbour",
             )
-
         subjects_commands.append(
             build_probtrackx2_arguments(
                 arg,
@@ -123,15 +116,18 @@ def pre_processing(arg: dict, handler) -> None:
                 ptx_options=arg["ptx_options"],
             )
         )
-
     # This supresses the signit kill message or else it prints it off multiple times for each core
     if arg["n_cores"]:
         handler.set_suppress_messages = True
-
     # Running probtrackx2
     Probtrackx(subjects_commands, arg["cluster"], arg["n_cores"])
+    print(arg)
     if arg["surface"]:
         [
-            update_seeds_file(os.path.join(sub, arg["outdir"], "seeds.txt"))
+            update_seeds_file(
+                os.path.join(
+                    arg["outdir"], "nfact_pp", os.path.basename(sub), "seeds.txt"
+                )
+            )
             for sub in arg["list_of_subjects"]
         ]
