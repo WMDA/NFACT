e7530e57a18c51c983c8eff5d9e439294e4c16f0
wmda <daniel.halls@nottingham.ac.uk>
2025-01-06 12:17:56 +0000

    linting

--- NFACT/base/cluster_support.py
+++ NFACT/base/cluster_support.py
@@ -2,6 +2,7 @@ from .utils import colours, error_and_exit
 import os
 import subprocess
 
+
 class Cluster_parameters:
     """
     Class to process cluster parameters.
@@ -13,37 +14,36 @@ class Cluster_parameters:
     Parameters
     ----------
     arg: dict
-        command line arguments    
+        command line arguments
     """
-    
-    def __init__(self, arg: dict):
 
+    def __init__(self, arg: dict):
         self.args = arg
         self.colours = colours()
         self.queues = self.has_queues()
-    
+
     def process_parameters(self):
         """
         Method to process parameters
-        
+
         Parameters
         ----------
         None
-    
+
         Returns
         ------
         args: dict
-        
+
         dictionary of processed
         command line arguments
         """
-        
+
         queues_avail = self.queues["stdout"] != "No"
         if not queues_avail:
             raise NoClusterQueuesException
         self.cluster_ram()
         self.cluster_queue_assignment()
-        if self.arg['cluster_qos']:
+        if self.arg["cluster_qos"]:
             self.cluster_qos()
         self.print_cluster_queues()
         return self.arg
@@ -52,48 +52,55 @@ class Cluster_parameters:
         """
         Method to check if queues
         available.
-    
-        Parameters
-        ----------
-        None
-    
-        Returns
-        -------
-        dict: dictionary
-            dictionary of fsl_sub call
         """
         return run_fsl_sub(
             [os.path.join(os.environ["FSLDIR"], "bin", "fsl_sub"), "--has_queues"]
         )
-    
+
     def cluster_ram(self):
         """Method to assign ram amount"""
-        self.arg["cluster_ram"] = self.arg["cluster_ram"] if self.arg["cluster_ram"] else 30
+        self.arg["cluster_ram"] = (
+            self.arg["cluster_ram"] if self.arg["cluster_ram"] else 30
+        )
 
     def cluster_time(self):
         """Method to assign cluster time"""
         self.arg["cluster_time"] = (
-            self.arg["cluster_time"] if self.arg["cluster_time"] else 160 if self.arg["gpu"] else 600
+            self.arg["cluster_time"]
+            if self.arg["cluster_time"]
+            else 160
+            if self.arg["gpu"]
+            else 600
         )
+
     def cluster_queue_assignment(self):
         """Method to assign cluster queue"""
         self.arg["cluster_queue"] = (
-            self.arg["cluster_queue"] if self.arg["cluster_queue"] in self.queues['std_out'] else None
+            self.arg["cluster_queue"]
+            if self.arg["cluster_queue"] in self.queues["std_out"]
+            else None
         )
+
     def cluster_qos(self):
         """Method to assign cluster qos"""
-        self.args['cluster_qos'] = f'--extra "--qos={self.args['cluster_qos']}'
-        
+        self.args["cluster_qos"] = f'--extra "--qos={self.args['cluster_qos']}'
+
     def print_cluster_queues(self):
-       """Method to print cluster queues"""
-       print_string = "No queue given will assign" if not self.arg["cluster_queue"] else self.arg["cluster_queue"]
-       print(f"{self.col['plum']}Cluster:{self.col['reset']} {print_string}")
+        """Method to print cluster queues"""
+        print_string = (
+            "No queue given will assign"
+            if not self.arg["cluster_queue"]
+            else self.arg["cluster_queue"]
+        )
+        print(f"{self.col['plum']}Cluster:{self.col['reset']} {print_string}")
 
 
 class NoClusterQueuesException(Exception):
     """Custom exception for when no cluster queues are available."""
+
     def __init__(self):
         super().__init__()
+
 
 def no_cluster_queues():
     """
@@ -111,9 +118,10 @@ def no_cluster_queues():
     """
     col = colours()
     print(
-            f"{col['plum']}Cluster:{col['reset']} No queues detected. Not running on cluster"
-        )
+        f"{col['plum']}Cluster:{col['reset']} No queues detected. Not running on cluster"
+    )
     return None
+
 
 def run_fsl_sub(command: list) -> dict:
     """
